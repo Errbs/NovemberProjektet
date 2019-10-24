5f90c2b54e5fd356ade952ed31874e5e62492352
Errbs <54944859+Errbs@users.noreply.github.com>
2019-10-24 15:45:44 +0200

    Second commit
    The base

---
+++ Character.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovProj
+{
+    class Character
+    {
+        protected string name;
+        protected Inventory inventory;
+        protected int hp;
+        protected int mp;
+        protected int strenght;
+        protected int x;
+        protected int y;
+
+        public void AddToInventory(Item item)
+        {
+
+        }
+        public void RemoveFromInventory(int n)
+        {
+
+        }
+        public void UseItem(int n, Character target)
+        {
+
+        }
+        public void UseItem(int n)
+        {
+
+        }
+        public int GetInventoryLenght()
+        {
+
+        }
+        public string GetItemInfo(int n)
+        {
+
+        }
+        public void Move(int xMove, int yMove)
+        {
+
+        }
+        public void SetPosition(int xPos, int yPos)
+        {
+
+        }
+        public int GetX()
+        {
+
+        }
+        public int GetY()
+        {
+
+        }
+        public int GetHp()
+        {
+
+        }
+        public int ModifyHp(int amount)
+        {
+
+        }
+        public void Attack(Character target)
+        {
+
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovProj
+{
+    class Item
+    {
+        protected string name;
+        public string GetInfo()
+        {
+
+        }
+        public void Use(Character target)
+        {
+
+        }
+
+    }
+}
