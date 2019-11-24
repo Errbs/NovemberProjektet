0a26085efa80acffafea3ad0e119a8442f2ddbb5
Errbs <54944859+Errbs@users.noreply.github.com>
2019-11-24 18:45:25 +0100

    14th commit (Main Code + Item subclass)
    Jag skrev mer kod i main som slumpar två tal, som sparas i int variabler isX och isY, sedan kollar jag om dessa värden stämmer överens med spelarens position och om den gör det hittar man nytt item.
    Jag skapade även en ny subklass till Item.

---
+++ Cursed.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovProj
+{
+    class Cursed : Item
+    {
+        static Random typeOfItem = new Random();
+        protected List<string> cursedItems = new List<string>() {"Pendant", "Book", "Glasses" };
+        protected int listIndex;
+
+        public void RandomizeItemType()
+        {
+            listIndex = typeOfItem.Next(cursedItems.Count);
+            name = "Cursed " + cursedItems[listIndex];
+        }
+
+        public override string GetInfo()
+        {
+            return name + ": is an mysterious cursed item and will punish you whenever you are in contact with it.";
+        }
+
+        public override void Use(Character target)
+        {
+            
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,55 +10,87 @@ namespace NovProj
     {
         static void Main(string[] args)
         {
-            Character test = new Character(); //Creates an instance of the class Character.
+            Character test = new Character(); // Creates an instance of the class Character.
                         
-            bool movePlayer;
+            bool movement = true; // Bool used to loop the code for a characters movement
 
-            Item potion = new Healthpotion();
+            Item potion = new Healthpotion(); // Creates an instance of the Item subclass Healthpotion
 
             
-            Console.WriteLine(potion.GetInfo()); 
-            potion.Use(test);
-            Console.WriteLine(test.GetHp()); 
 
-            movePlayer = true;
-
+                                    
+            Console.WriteLine(potion.GetInfo()); // Writes out info from the instance potions GetInfo method.
+            potion.Use(test); // Applies the effects of the item onto the Character instance test.
+            Console.WriteLine("Current HP: " + test.GetHp()); // Displays the new HP
+                       
           
-            while (movePlayer == true)
+            while (movement == true)
             {
-                ConsoleKeyInfo keyInfo = Console.ReadKey();
-                ConsoleKey key = keyInfo.Key;
+                int isX;
+                int isY;
+                Random generateNumber = new Random();
+                isX = generateNumber.Next();
+                isY = generateNumber.Next();
+                ConsoleKeyInfo keyInfo = Console.ReadKey(); // Takes input
+                Console.Clear();
+                ConsoleKey key = keyInfo.Key; // Translates input into the corresponding keyboard key.
 
                 if (key == ConsoleKey.A)
                 {
-                    test.Move(-1, 0);
-                    Console.WriteLine(test.GetX());
+                    test.Move(-1, 0); // Runs the class Characters, instance tests Move method with the inputs -1, 0.
+                    test.SetPosition(test.GetX(), test.GetY()); // Takes the values from GetX and GetY methods in class Character, instance test and uses those values as the xPos, yPos parameters for the Character method SetPosition.
+                    Console.WriteLine("X:" + test.GetX());  // Writes out information from the GetX method in class Character, for the instance test.
 
                 }
                 if (key == ConsoleKey.D)
                 {
                       
-                    test.Move(+1,0);
-                    Console.WriteLine(test.GetX());                            
+                    test.Move(+1,0); // Runs the class Characters, instance tests Move method with the inputs +1, 0.
+                    test.SetPosition(test.GetX(), test.GetY()); // Takes the values from GetX and GetY methods in class Character, instance test and uses those values as the xPos, yPos parameters for the Character method SetPosition.
+                    Console.WriteLine("X:" + test.GetX()); // Writes out information from the GetX method in class Character, for the instance test.                       
                 }
                 if (key == ConsoleKey.W)
                 {
                       
-                    test.Move(0,+1);
-                    Console.WriteLine(test.GetY());
+                    test.Move(0,+1); // Runs the class Characters, instance tests Move method with the inputs 0, +1.
+                    test.SetPosition(test.GetX(), test.GetY()); // Takes the values from GetX and GetY methods in class Character, instance test and uses those values as the xPos, yPos parameters for the Character method SetPosition.
+                    Console.WriteLine("Y:" + test.GetY()); // Writes out information from the GetY method in class Character, for the instance test.
                 }
                 if (key == ConsoleKey.S)
                 {
-                    test.Move(0,-1);
-                    Console.WriteLine(test.GetY());
+                    test.Move(0,-1); // Runs the class Characters, instance tests Move method with the inputs 0, -1.
+                    test.SetPosition(test.GetX(), test.GetY()); // Takes the values from GetX and GetY methods in class Character, instance test and uses those values as the xPos, yPos parameters for the Character method SetPosition.
+                    Console.WriteLine("Y:" + test.GetY()); // Writes out information from the GetY method in class Character, for the instance test.
                                    
                 }
-                else if(key!=ConsoleKey.S&&key!=ConsoleKey.A&&key!=ConsoleKey.W&&key!=ConsoleKey.D)
+                if (test.GetX() == isX && test.GetY() == isY)
+                {
+                    int randomizedItemFound = generateNumber.Next(1, 3);
+                    if(randomizedItemFound == 1)
+                    {
+                        Console.WriteLine("Healthpotion Found!");
+                        test.AddToInventory(potion);
+                        test.GetItemInfo(randomizedItemFound);
+
+                    }
+                    else if(randomizedItemFound == 2)
+                    {
+                        Console.WriteLine(" Found!");
+                        test.AddToInventory();
+                        test.GetItemInfo(randomizedItemFound);
+                    }
+                    else if (randomizedItemFound == 3)
+                    {
+                        Console.WriteLine(" Found!");
+                        test.AddToInventory();
+                        test.GetItemInfo(randomizedItemFound);
+                    }
+                }
+                else if (key != ConsoleKey.S && key != ConsoleKey.A && key != ConsoleKey.W && key != ConsoleKey.D)
                 {
                     Console.WriteLine("that's not a move key, use WASD.");
                 }
-
-                keyInfo = Console.ReadKey();
+                // This else if sends an error message if the player presses other keys than WASD.
 
                 /*
                 This while loop will loop when the bool movePlayer is true, which I'll define later. 
